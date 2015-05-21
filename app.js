5db034576a21afe1303a8bbef3aa821af9f25905
Breno Souza <breno.ec@gmail.com>
2015-05-21 15:10:19 -0300

    morgan now logs only error messages

--- app.js
+++ app.js
@@ -31,7 +31,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
+
+//app.use(logger('dev'));
+app.use(logger('combined', {
+  skip: function (req, res) { return res.statusCode < 400; }
+}));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
