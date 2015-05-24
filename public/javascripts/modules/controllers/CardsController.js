e404bca169fe15d654cf42f0325caafb7c9047e8
Breno Souza <breno.ec@gmail.com>
2015-05-24 12:20:35 -0300

    cards.jade refactor

--- public/javascripts/modules/controllers/CardsController.js
+++ public/javascripts/modules/controllers/CardsController.js
@@ -1,43 +1,28 @@
 (function() {
 
   angular.module('store').controller('CardsController', ['$scope', '$http', '$location', '$anchorScroll', function($scope, $http, $location, $anchorScroll) {
-    
-    var data = {};
-    
+
     $scope.selected = {};
-    
+
     $scope.random = false;
-    
-    // pagination
+
+    // pagination variables
     $scope.items = 10;
     $scope.page = 1;
     $scope.lastPage = 1;
-    
+
     $scope.query = '';
-    
-    $scope.setSelectedCardItem = function(card, item) {
-      $scope.selected[card.searchName + card.id] = item;
-    };
-    
+
     $scope.setCardsCollection = function(data, skip, limit) {
       $scope.cards = data.cards;
       $scope.count = data.count;
       $scope.page = 1 + skip / limit;
       $scope.lastPage = Math.ceil($scope.count / limit);
-      
-      for (var i = 0; i < $scope.cards.length; i++) {
-        if ($scope.cards[i].stock.length) {
-          for (var j = 0; j < $scope.cards[i].stock.length; j++) {
-            if ($scope.cards[i].stock[j].quantity > 0) {
-              $scope.setSelectedCardItem($scope.cards[i], $scope.cards[i].stock[j]);
-              break;
-            }
-          }
-        }
-      }
+
+      // clear selected items
+      $scope.selected = {};
     };
-    
-    // search bar
+
     $scope.search = function(query, skip, limit, random) {
       $http.get('/api/cards', {
           cache: true,
@@ -53,10 +38,82 @@
           $anchorScroll();
         });
     };
-    
+
     // retrieve data
     $scope.search('', ($scope.page - 1) * $scope.items, $scope.items, $scope.random);
-    
+
+    $scope.getDisplayMode = function() {
+      if ($scope.item === 1) {
+        return 'show-one';
+      }
+
+      return '';
+    };
+
+    $scope.hasSelectedProdutcItemDiscount = function(product) {
+      var item = $scope.selected[product.searchName + product.id];
+
+      if (item && item.discount) {
+        return true;
+      }
+
+      return false;
+    };
+
+    // returns the selected item
+    $scope.setSelectedProdutcItem = function(product, item) {
+
+      // if an item as given, set it selected
+      if (item) {
+        return $scope.selected[product.searchName + product.id] = item;
+      }
+
+      var items = [];
+      var itemsWithDiscount = [];
+
+      // sort avaible items by discount
+      items = _.sortBy(product.stock, 'discount').reverse();
+      items = _.filter(items, function(item) {
+        return item.quantity > 0;
+      });
+
+      if (!items.length) {
+        return null;
+      }
+
+      itemsWithDiscount = _.filter(items, function(item) {
+        return item.discount > 0;
+      });
+
+      // select discounted item
+      if (itemsWithDiscount.length) {
+        return $scope.selected[product.searchName + product.id] = itemsWithDiscount[0];
+      }
+
+      // return an avaible item
+      return $scope.selected[product.searchName + product.id] = items[0];
+    };
+
+    $scope.isProdutcAvaible = function(product) {
+      if ($scope.selected[product.searchName + product.id]) {
+        return $scope.selected[product.searchName + product.id];
+      }
+
+      if (product && product.stock.length > 0) {
+        return $scope.setSelectedProdutcItem(product, null);
+      }
+
+      return false;
+    };
+
+    $scope.isSelected = function(product, item) {
+      if ($scope.selected[product.searchName + product.id] === item) {
+        return true;
+      }
+
+      return false;
+    };
+
   }]);
-  
+
 })();
