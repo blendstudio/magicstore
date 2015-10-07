749e0deab7b10cb293359066f58716316351ad2e
Breno Souza <breno.ec@gmail.com>
2015-10-07 13:00:54 -0300

    api/cards using mongoose-finder module

--- routes/api/products/cards.js
+++ routes/api/products/cards.js
@@ -1,56 +1,20 @@
 (function() {
-  var express = require('express');
-  var router = express.Router();
+  var mongooseFinder = require('../../../modules/mongoose-finder'),
+            mongoose = require('mongoose'),
+               model = require('../../../models/Card'),
 
-  var mongoose = require('mongoose');
-  var Model = Card = require('../../../models/Card');
+             express = require('express'),
+              router = express.Router(),
 
-  var _ = require('lodash');
+                   _ = require('lodash');
 
   /* GET products */
   router.get('/', function(req, res, next) {
-    var q        = {},                                  // mongoose query object
-        query    = JSON.parse(req.query.query),         // mongoose query
-        property = '',
-        count    = 0;
+    var query = JSON.parse(req.query.query);
 
-    // apply like operator
-    for (var i = 0; i < query['search-options'].like.length; i++) {
-      property = query['search-options'].like[i];
-      if (query.conditions[property]) {
-        query.conditions[property] = new RegExp(query.conditions[property], 'i');
-      }
-    }
-
-    var chain = [
-      // get count
-      function() {
-        q = Model.count(query.conditions);
-        q.exec(chain.shift());
-      },
-
-      // get models
-      function(err, data) {
-        if (err) { throw err; }
-
-        count = data;
-        if (query['query-options'].random === true) {
-          q = Model.findRandom(query.conditions, query.projection, query.options);
-        } else {
-          q = Model.find(query.conditions, query.projection, query.options);
-        }
-
-        q.exec(chain.shift());
-      },
-
-      // return response
-      function(err, data) {
-        if (err) { throw err; }
-        res.json({ values: data, count: count });
-      },
-    ];
-
-    chain.shift()();
+    mongooseFinder.find(model, query, function(data, count) {
+      res.json({ values: data, count: count });
+    });
   });
 
   module.exports = router;
