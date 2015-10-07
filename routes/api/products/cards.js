0d90d3f4c942a1643cefdd183d00f824a9ffe8d7
Breno Souza <breno.ec@gmail.com>
2015-10-07 00:18:21 -0300

    minor refactor

--- routes/api/products/cards.js
+++ routes/api/products/cards.js
@@ -1,57 +1,57 @@
-var express = require('express');
-var router = express.Router();
+(function() {
+  var express = require('express');
+  var router = express.Router();
 
-var mongoose = require('mongoose');
-var Model = Card = require('../../../models/Card');
+  var mongoose = require('mongoose');
+  var Model = Card = require('../../../models/Card');
 
-var _ = require('lodash');
+  var _ = require('lodash');
 
-/* GET products */
-router.get('/', function(req, res, next) {
-  // mongoose query object
-  var q = {};
-  // mongoose query
-  var query = JSON.parse(req.query.query);
+  /* GET products */
+  router.get('/', function(req, res, next) {
+    var q        = {},                                  // mongoose query object
+        query    = JSON.parse(req.query.query),         // mongoose query
+        property = '',
+        count    = 0;
 
-  // apply like operator
-  var property = '';
-  for (var i = 0; i < query['search-options'].like.length; i++) {
-    property = query['search-options'].like[i];
-    if (query.conditions[property]) {
-      query.conditions[property] = new RegExp(query.conditions[property], 'i');
-    }
-  }
-
-  var count = 0;
-  var chain = [
-    // get count
-    function() {
-      q = Model.count(query.conditions);
-      q.exec(chain.shift());
-    },
-
-    // search for models
-    function(err, data) {
-      if (err) { throw err; }
-
-      count = data;
-      if (query['query-options'].random === true) {
-        q = Model.findRandom(query.conditions, query.projection, query.options);
-      } else {
-        q = Model.find(query.conditions, query.projection, query.options);
+    // apply like operator
+    for (var i = 0; i < query['search-options'].like.length; i++) {
+      property = query['search-options'].like[i];
+      if (query.conditions[property]) {
+        query.conditions[property] = new RegExp(query.conditions[property], 'i');
       }
+    }
 
-      q.exec(chain.shift());
-    },
+    var chain = [
+      // get count
+      function() {
+        q = Model.count(query.conditions);
+        q.exec(chain.shift());
+      },
 
-    // return response
-    function(err, data) {
-      if (err) { throw err; }
-      res.json({ values: data, count: count });
-    },
-  ];
+      // get models
+      function(err, data) {
+        if (err) { throw err; }
 
-  chain.shift()();
-});
+        count = data;
+        if (query['query-options'].random === true) {
+          q = Model.findRandom(query.conditions, query.projection, query.options);
+        } else {
+          q = Model.find(query.conditions, query.projection, query.options);
+        }
 
-module.exports = router;
+        q.exec(chain.shift());
+      },
+
+      // return response
+      function(err, data) {
+        if (err) { throw err; }
+        res.json({ values: data, count: count });
+      },
+    ];
+
+    chain.shift()();
+  });
+
+  module.exports = router;
+})();
